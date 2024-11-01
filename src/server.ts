e2f4f543b8df929127e1af5a16784cba02b0add3
Joao Tapparo <joaotapparo@protonmail.com>
2024-11-01 18:39:34 -0300

    making getProductById and Delete routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { Request, Response, Router } from "express";
 import { ProductsRepository } from "./ProductsRepository";
-import { Product } from "./product"; // certifique-se de que o caminho esteja correto
+import { Product } from "./product";
 
 const app = express();
 const port = 3000;
@@ -39,6 +39,60 @@ routes.post("/addProduct", async (req: Request, res: Response) => {
   try {
     const createdProduct = await productsRepo.create(newProduct);
     res.status(201).json(createdProduct);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+// rota para obter um produto pelo id
+routes.get("/getProduct/:id", async (req: Request, res: Response) => {
+  const productId = parseInt(req.params.id, 10);
+
+  try {
+    const product = await productsRepo.getById(productId);
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ message: "Produto não encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+// rota para deletar um produto
+routes.delete("/deleteProduct/:id", async (req: Request, res: Response) => {
+  const productId = parseInt(req.params.id, 10);
+
+  try {
+    const deletedRows = await productsRepo.delete(productId);
+    if (deletedRows > 0) {
+      res.status(200).json({ message: "Produto deletado com sucesso" });
+    } else {
+      res.status(404).json({ message: "Produto não encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+// rota para atualizar um produto
+routes.put("/updateProduct/:id", async (req: Request, res: Response) => {
+  const productId = parseInt(req.params.id, 10);
+  const updatedProduct: Product = {
+    id: productId,
+    name: req.body.name as string,
+    price: parseFloat(req.body.price as string),
+    description: req.body.description as string,
+  };
+
+  try {
+    const product = await productsRepo.update(updatedProduct);
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ message: "Produto não encontrado" });
+    }
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
   }
