093a5c806c7bf91e77ce161040f48aedee32ea4e
Joao Tapparo <joaotapparo@protonmail.com>
2024-11-01 18:45:30 -0300

    switching query from MySQL to Redis

--- src/ProductsRepository.ts
+++ src/ProductsRepository.ts
@@ -6,17 +6,27 @@ export class ProductsRepository {
   // retorna todos os produtos
   async getAll(): Promise<Product[]> {
     return new Promise((resolve, reject) => {
-      conn.query<RowDataPacket[]>("SELECT * FROM PRODUCTS", (err, res) => {
+      redis.get("products:all", (err, cachedProducts) => {
         if (err) return reject(err);
+        if (cachedProducts) {
+          return resolve(JSON.parse(cachedProducts));
+        }
 
-        // mapeia os resultados para um array de produtos
-        const products: Product[] = res.map((row) => ({
-          id: row.id,
-          name: row.name,
-          price: row.price,
-          description: row.description,
-        }));
-        resolve(products);
+        conn.query<RowDataPacket[]>("SELECT * FROM PRODUCTS", (err, res) => {
+          if (err) return reject(err);
+
+          // mapeia os resultados para um array de produtos
+          const products: Product[] = res.map((row) => ({
+            id: row.id,
+            name: row.name,
+            price: row.price,
+            description: row.description,
+          }));
+
+          // armazena os produtos no cache
+          redis.set("products:all", JSON.stringify(products), "EX", 3600);
+          resolve(products);
+        });
       });
     });
   }
@@ -24,25 +34,42 @@ export class ProductsRepository {
   // retorna um produto pelo id
   async getById(product_id: number): Promise<Product | undefined> {
     return new Promise((resolve, reject) => {
-      conn.query<RowDataPacket[]>(
-        "SELECT * FROM PRODUCTS WHERE id = ?",
-        [product_id],
-        (err, res) => {
-          if (err) return reject(err);
-
-          // converte a linha em um produto
-          const product =
-            res.length > 0
-              ? {
-                  id: res[0].id,
-                  name: res[0].name,
-                  price: res[0].price,
-                  description: res[0].description,
-                }
-              : undefined;
-          resolve(product);
+      redis.get(`product:${product_id}`, (err, cachedProduct) => {
+        if (err) return reject(err);
+        if (cachedProduct) {
+          return resolve(JSON.parse(cachedProduct));
         }
-      );
+
+        conn.query<RowDataPacket[]>(
+          "SELECT * FROM PRODUCTS WHERE id = ?",
+          [product_id],
+          (err, res) => {
+            if (err) return reject(err);
+
+            // converte a linha em um produto
+            const product =
+              res.length > 0
+                ? {
+                    id: res[0].id,
+                    name: res[0].name,
+                    price: res[0].price,
+                    description: res[0].description,
+                  }
+                : undefined;
+
+            // armazena o produto no cache
+            if (product) {
+              redis.set(
+                `product:${product_id}`,
+                JSON.stringify(product),
+                "EX",
+                3600
+              );
+            }
+            resolve(product);
+          }
+        );
+      });
     });
   }
 
